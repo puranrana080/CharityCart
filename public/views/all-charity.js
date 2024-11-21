0423a0a4843e1ec8519836e84868334141028365
Puran Singh Rana <puransinghrana080@gmail.com>
2024-11-21 21:46:39 +0530

    search added

--- public/views/all-charity.js
+++ public/views/all-charity.js
@@ -1,33 +1,39 @@
 
 
-window.addEventListener('DOMContentLoaded',()=>{
-    const token=localStorage.getItem('token')
+window.addEventListener('DOMContentLoaded', () => {
+    const token = localStorage.getItem('token')
 
-    axios.get('http://localhost:3000/cherities',{headers:{"Authorization":token}})
-    .then(response=>{
-        console.log("all checrities",response.data.cherities)
-        const charities=response.data.cherities
-        console.log("sa",charities)
+    axios.get('http://localhost:3000/cherities', { headers: { "Authorization": token } })
+        .then(response => {
+            console.log("all checrities", response.data.cherities)
+            const charities = response.data.cherities
+            console.log("sa", charities)
 
-        charities.forEach((charity) => {
-            console.log(charity)
-            displayAllCharities(charity)
+            charities.forEach((charity) => {
+                console.log(charity)
+                displayAllCharities(charity)
+            })
+            //search filter
+            const search = document.querySelector('.search')
+            search.addEventListener('keyup', () => {
+                const word = search.value.toLowerCase()
+                filterCharities(charities, word)
+            })
         })
-    })
-    .catch(err=>{
-        console.log(err)
-    })
+        .catch(err => {
+            console.log(err)
+        })
 
 })
 
 
-function displayAllCharities(charity){
+function displayAllCharities(charity) {
 
-    const list =document.querySelector('.box-container')
+    const list = document.querySelector('.box-container')
 
 
-    const col=document.createElement('div')
-    col.innerHTML=`<div class="card" style="width: 18rem;">
+    const col = document.createElement('div')
+    col.innerHTML = `<div class="card" style="width: 18rem;">
             <img src="/images/cardimg.avif" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${charity.name}</h5>
@@ -37,13 +43,22 @@ function displayAllCharities(charity){
           </div>`
     list.appendChild(col)
 
-    col.querySelector('.donate-btn').addEventListener('click',()=>{    
-            window.location.href = `/views/charity-profile.html?charityId=${charity.id}`
+    col.querySelector('.donate-btn').addEventListener('click', () => {
+        window.location.href = `/views/charity-profile.html?charityId=${charity.id}`
     })
-
-
 }
+function filterCharities(charities,word){
+    const list=document.querySelector('.box-container')
+    list.innerHTML=''
 
+    const filteredCharities=charities.filter(charity=>
+        charity.name.toLowerCase().includes(word)||
+        charity.location.toLowerCase().includes(word)||
+        charity.category.toLowerCase().includes(word)
+    )
 
-
+    filteredCharities.forEach(charity=>
+        displayAllCharities(charity)
+    )
+}
 
